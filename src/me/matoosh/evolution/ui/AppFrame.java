d3e89ed6d3043a9ae8f21b55178aa6281beb0eb7
itsMatoosh <matirebacz@op.pl>
2017-05-20 21:53:36 +0200

    Added modules to the AppFrame

--- src/me/matoosh/evolution/ui/AppFrame.java
+++ src/me/matoosh/evolution/ui/AppFrame.java
@@ -1,6 +1,9 @@
 package me.matoosh.evolution.ui;
 
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JFrame;
 
 /**
@@ -13,6 +16,15 @@ public class AppFrame extends JFrame {
 	 * UUID of this ui element.
 	 */
 	private static final long serialVersionUID = 928370391529452127L;
+	
+	/**
+	 * The simulation options panel.
+	 */
+	private SimulationOptionsPanel simulationOptionsPanel;
+	/**
+	 * The simulation panel.
+	 */
+	private SimulationPanel simulationPanel;
 	
 	/**
 	 * Instantiates a default application frame.
@@ -32,9 +44,15 @@ public class AppFrame extends JFrame {
 	}
 	
 	/**
-	 * Adds the default panels to thw window.
+	 * Adds the default panels to the window.
 	 */
 	private void addPanels() {
-		
+		//Simulation panel.
+		simulationPanel = new SimulationPanel();
+		add(simulationPanel, new GridBagConstraints(0, 0, 1, 1, 0.95, 1, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+	
+		//Settings panel.
+		simulationOptionsPanel = new SimulationOptionsPanel();
+		add(simulationOptionsPanel, new GridBagConstraints(1, 0, 1, 1, 0.05, 1, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
 	}
 }
